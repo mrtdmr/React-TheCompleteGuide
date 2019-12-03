43f15679a496d83ef0fcc9c8202b37ececaf2ecc
murat <muratdemir.pau@gmail.com>
2019-12-03 23:56:17 +0300

    03122019_2356

--- burger-app/src/components/Burger/BuildControls/BuildControls.js
+++ burger-app/src/components/Burger/BuildControls/BuildControls.js
@@ -9,9 +9,24 @@ const controls = [
 ];
 const buildControls = props => (
   <div className={classes.BuildControls}>
+    <p>
+      Current Price:<strong> {props.price.toFixed(2)}</strong>
+    </p>
     {controls.map(ctrl => {
-      return <BuildControl key={ctrl.label} label={ctrl.label} />;
+      return (
+        <BuildControl
+          key={ctrl.label}
+          label={ctrl.label}
+          type={ctrl.type}
+          added={() => props.ingredientAdded(ctrl.type)}
+          removed={() => props.ingredientRemoved(ctrl.type)}
+          disabled={props.disabled[ctrl.type]}
+        />
+      );
     })}
+    <button className={classes.OrderButton} disabled={!props.purchasable}>
+      OrDER NOW
+    </button>
   </div>
 );
 export default buildControls;
