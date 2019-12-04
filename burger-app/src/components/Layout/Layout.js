097c899d78c60e3c7ace95aa564e366cd9bfac0a
murat <muratdemir.pau@gmail.com>
2019-12-05 00:00:32 +0300

    05122019

--- burger-app/src/components/Layout/Layout.js
+++ burger-app/src/components/Layout/Layout.js
@@ -1,9 +1,12 @@
 import React from "react";
 import Aux from "../../hoc/Auxiliary";
 import styles from "./Layout.module.css";
+import Toolbar from "../../components/Navigation/Toolbar/Toolbar";
+import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
 const layout = props => (
   <Aux>
-    <div>Toolbar,SideDrawer,Backdrop</div>
+    <Toolbar />
+    <SideDrawer />
     <main className={styles.Content}>{props.children}</main>
   </Aux>
 );
