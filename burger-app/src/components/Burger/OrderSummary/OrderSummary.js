30abb508d3b6f0adfee78dc5e8a4d4b941e1b58c
murat <muratdemir.pau@gmail.com>
2019-12-06 00:33:28 +0300

    06122019_0033

--- burger-app/src/components/Burger/OrderSummary/OrderSummary.js
+++ burger-app/src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,32 +1,37 @@
-import React from "react";
-import Aux from "../../../hoc/Auxiliary";
+import React, { Component } from "react";
+import Aux from "../../../hoc/Auxiliary/Auxiliary";
 import Button from "../../UI/Button/Button";
-const orderSummary = props => {
-  const ingredientSummary = Object.keys(props.ingredients).map(igKey => {
+class OrderSummary extends Component {
+  //Functional component olabilirdi. Lifecycle için class componenete çevrildi.
+  componentDidUpdate() {
+    console.log("[OrderSummary] did update");
+  }
+  render() {
+    const ingredientSummary = Object.keys(this.props.ingredients).map(igKey => {
+      return (
+        <li key={igKey}>
+          <span style={{ textTransform: "capitalize" }}> {igKey}</span>:
+          {this.props.ingredients[igKey]}
+        </li>
+      );
+    });
     return (
-      <li key={igKey}>
-        <span style={{ textTransform: "capitalize" }}> {igKey}</span>:
-        {props.ingredients[igKey]}
-      </li>
+      <Aux>
+        <h3>Your Order</h3>
+        <p>A delicious burger with the following ingredients:</p>
+        <ul>{ingredientSummary}</ul>
+        <p>
+          <strong>Total Price:{this.props.price.toFixed(2)}</strong>
+        </p>
+        <p>Continue to Checkout?</p>
+        <Button btnType="Danger" clicked={this.props.purchaseCanceled}>
+          CANCEL
+        </Button>
+        <Button btnType="Success" clicked={this.props.purchaseContinued}>
+          CONTINUE
+        </Button>
+      </Aux>
     );
-  });
-
-  return (
-    <Aux>
-      <h3>Your Order</h3>
-      <p>A delicious burger with the following ingredients:</p>
-      <ul>{ingredientSummary}</ul>
-      <p>
-        <strong>Total Price:{props.price.toFixed(2)}</strong>
-      </p>
-      <p>Continue to Checkout?</p>
-      <Button btnType="Danger" clicked={props.purchaseCanceled}>
-        CANCEL
-      </Button>
-      <Button btnType="Success" clicked={props.purchaseContinued}>
-        CONTINUE
-      </Button>
-    </Aux>
-  );
-};
-export default orderSummary;
+  }
+}
+export default OrderSummary;
--- burger-app/src/components/UI/Backdrop/Backdrop.js
+++ burger-app/src/components/UI/Backdrop/Backdrop.js
@@ -2,8 +2,6 @@ import React from "react";
 import classes from "./Backdrop.module.css";
 const backdrop = props =>
   props.show ? (
-    <div className={classes.Backdrop} onClick={props.clicked}>
-      xxx
-    </div>
+    <div className={classes.Backdrop} onClick={props.clicked}></div>
   ) : null;
 export default backdrop;
