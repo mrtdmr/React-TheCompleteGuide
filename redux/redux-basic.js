30bc0d386baedf986483a03e31a16746a38426e2
mrtdmr <muratdemir.pau@gmail.com>
2019-12-26 16:45:35 +0300

    261219_1645

--- redux/redux-basic.js
+++ redux/redux-basic.js
@@ -4,6 +4,18 @@ const initialState = { counter: 0 };
 
 //Reducer
 const rootReducer = (state = initialState, action) => {
+  if (action.type === "INC_COUNTER") {
+    return {
+      ...state,
+      counter: state.counter + 1
+    };
+  }
+  if (action.type === "ADD_COUNTER") {
+    return {
+      ...state,
+      counter: state.counter + action.value
+    };
+  }
   return state;
 };
 
@@ -11,7 +23,12 @@ const rootReducer = (state = initialState, action) => {
 const store = createStore(rootReducer);
 console.log(store.getState());
 
-//Dispatching Action
-store.dispatch({ type });
-
 //Subscription
+store.subscribe(() => {
+  console.log("[Subscription]", store.getState());
+});
+
+//Dispatching Action
+store.dispatch({ type: "INC_COUNTER" });
+store.dispatch({ type: "ADD_COUNTER", value: 10 });
+console.log(store.getState());
