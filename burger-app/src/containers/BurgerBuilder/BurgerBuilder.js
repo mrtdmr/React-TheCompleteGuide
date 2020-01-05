52b250df018fe018667f1ff8bdf4820b2c85ac6d
murat <muratdemir.pau@gmail.com>
2020-01-05 22:45:35 +0300

    050119_2245

--- burger-app/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-app/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -8,7 +8,7 @@ import axios from "../../axios-orders";
 import Spinner from "../../components/UI/Spinner/Spinner";
 import withErrorHandler from "../../hoc/withErrorHandler/withErrorHandler";
 import { connect } from "react-redux";
-import * as actionTypes from "../../store/actions";
+import * as burgerBuilderActions from "../../store/actions/index";
 
 class BurgerBuilder extends Component {
   // constructor(props){
@@ -168,12 +168,9 @@ const mapStateToProps = state => {
 const mapDispatchToProps = dispatch => {
   return {
     onIngredientAdded: ingName =>
-      dispatch({ type: actionTypes.ADD_INGREDIENTS, ingredientName: ingName }),
+      dispatch(burgerBuilderActions.addIngredient(ingName)),
     onIngredientRemoved: ingName =>
-      dispatch({
-        type: actionTypes.REMOVE_INGREDIENTS,
-        ingredientName: ingName
-      })
+      dispatch(burgerBuilderActions.removeIngredient(ingName))
   };
 };
 
--- burger-app/src/store/reducers/burgerBuilder.js
+++ burger-app/src/store/reducers/burgerBuilder.js
@@ -1,4 +1,4 @@
-import * as actionTypes from "./actions";
+import * as actionTypes from "../actions/actionTypes";
 const initialState = {
   ingredients: { salad: 0, bacon: 0, cheese: 0, meat: 0 },
   totalPrice: 4
@@ -11,7 +11,7 @@ const INGREDIENT_PRICES = {
 };
 const reducer = (state = initialState, action) => {
   switch (action.type) {
-    case actionTypes.ADD_INGREDIENTS:
+    case actionTypes.ADD_INGREDIENT:
       return {
         ...state,
         ingredients: {
@@ -20,7 +20,7 @@ const reducer = (state = initialState, action) => {
         },
         totalPrice: state.totalPrice + INGREDIENT_PRICES[action.ingredientName]
       };
-    case actionTypes.REMOVE_INGREDIENTS:
+    case actionTypes.REMOVE_INGREDIENT:
       return {
         ...state,
         ingredients: {
