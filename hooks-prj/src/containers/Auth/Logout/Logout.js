9eedfcbce6da7caae97a551f295e39871db0d499
murat <muratdemir.pau@gmail.com>
2020-01-27 23:25:16 +0300

    270119

--- hooks-prj/src/containers/Auth/Logout/Logout.js
+++ hooks-prj/src/containers/Auth/Logout/Logout.js
@@ -1,23 +1,21 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import { Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import * as actions from '../../../store/actions/index';
 
-class Logout extends Component {
-    componentDidMount () {
-        this.props.onLogout();
-    }
-
-    render () {
-        return <Redirect to="/"/>;
-    }
-}
-
-const mapDispatchToProps = dispatch => {
-    return {
-        onLogout: () => dispatch(actions.logout())
-    };
+const logOut = props => {
+  const { onLogout } = props;
+  useEffect(() => {
+    onLogout();
+  }, [onLogout]);
+  return <Redirect to='/' />;
 };
 
-export default connect(null, mapDispatchToProps)(Logout);
+const mapDispatchToProps = dispatch => {
+  return {
+    onLogout: () => dispatch(actions.logout())
+  };
+};
+
+export default connect(null, mapDispatchToProps)(logOut);
